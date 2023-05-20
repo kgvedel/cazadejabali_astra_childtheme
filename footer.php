c0f03391dc14233128500ac990e826d1f8f16ea4
KatrineVedel <katrinevedel@hotmail.com>
2023-05-20 13:54:04 +0200

    d

--- footer.php
+++ footer.php
@@ -19,10 +19,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</div> <!-- ast-container -->
 	</div><!-- #content -->
 <?php 
-<head>
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-</head>
 
 <footer>
     <section class="left">

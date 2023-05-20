51064dc0bcedb905ae63170bac68574accacabb1
KatrineVedel <katrinevedel@hotmail.com>
2023-05-20 14:35:30 +0200

    mangler

--- footer.php
+++ footer.php
@@ -37,18 +37,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="footer_contact">
 		<ul>
-			<li><a href="https://goo.gl/maps/swjEAjDChhDWmxaz7"><span class="material-icons">location_on</span>CazadeJabalí, Calle San Miguel 37A, 22583 Arén, España</a></li>
+			<li><a href="https://goo.gl/maps/swjEAjDChhDWmxaz7" target="_blank"><span class="material-icons">location_on</span>CazadeJabalí, Calle San Miguel 37A, 22583 Arén, España</a></li>
 			<li><a href="tel:+34638731397"><span class="material-icons">phone</span>+34 638 731 397</a></li>
 			<li><a href="tel:+4530647409"><span class="material-icons">phone</span>+45 30 64 74 09</a></li>
-			<li><a href="mailto:tom@cazadejabali.com"><span class="material-icons">email</span>Tom@cazadejabali.com</a></li>
+			<li><a href="mailto:tom@cazadejabali.com" target="_blank"><span class="material-icons">email</span>Tom@cazadejabali.com</a></li>
 		</ul>
 	</div>
 </section>
 <div class="footer_social">
 <ul>
-<li><a href="https://www.facebook.com"><span class="fa fa-facebook-official-2x"></span></a></li>
-<li><a href="https://www.instagram.com"><span class="fa fa-instagram-2x"></span></a></li>
-<li><a href="https://www.youtube.com"><span class="fa fa-youtube-2x"></span></a></li>
+<li><a href="https://www.facebook.com" target="_blank"><span class="fa fa-facebook-official fa-2x"></span></a></li>
+<li><a href="https://www.instagram.com" target="_blank"><span class="fa fa-instagram fa-2x"></span></a></li>
+<li><a href="https://www.youtube.com" target="_blank"><span class="fa fa-youtube fa-2x"></span></a></li>
 </ul>
 </div>
 

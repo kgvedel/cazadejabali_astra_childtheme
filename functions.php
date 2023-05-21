cc68d17b0f7dba318153cd8bb64c3b5e81f781ae
mathilde <math68n8@stud.kea.dk>
2023-05-21 20:56:56 +0200

    new functions and new styles.css

--- functions.php
+++ functions.php
@@ -1,28 +1,6 @@
 <?php
-/**
- * astra-child-theme Theme functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package astra-child
- * @since 1.0.0
- */
-
-/**
- * Define Constants
- */
-define('CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0');
-
-/**
- * Enqueue styles
- */
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
-function my_theme_enqueue_styles()
-{
-	wp_enqueue_style(
-		'astra-child',
-		get_stylesheet_uri(),
-		array('astra'),
-		wp_get_theme()->get('Version')
-	);
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+?>
